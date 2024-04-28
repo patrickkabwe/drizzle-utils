ce74d3ecbee3917f926357b747d1d559f585ecd2
Patrick Kabwe <patrickckabwe@gmail.com>
2024-04-28 06:26:19 +0200

    chore: add test and build config

---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  outDir: "lib",
+  splitting: false,
+  sourcemap: false,
+  clean: true,
+  dts: true,
+  format: ["esm", "cjs"],
+});
